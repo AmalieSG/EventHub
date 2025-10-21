70eb9ad048b88df66af59d05ddb1f4417ef20f9f
Amalie <iagresho@hiof.no>
2025-10-21 12:14:23 +0200

    La til fetch/get for å hente et event basert på id

--- EventHub/src/app/api/events/eventsService.ts
+++ EventHub/src/app/api/events/eventsService.ts
@@ -1,6 +1,10 @@
-import { getAllEvents } from "./eventsRepository"
+import { getAllEvents, getEventById } from "./eventsRepository"
 import type { Event } from "@/app/types/event" 
 
 export async function fetchAllEvents(): Promise<Event[]> {
   return getAllEvents()
+}
+
+export async function fetchEventById(id: number): Promise<Event | undefined> {
+  return getEventById(id);
 }
