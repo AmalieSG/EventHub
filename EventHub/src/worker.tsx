fd7032fcfbe60f2fdace2e2c2f5d5f38cb1f39d8
Amalie <iagresho@hiof.no>
2025-11-08 21:28:08 +0100

    nullstilt d1 og kjørt seed

--- EventHub/src/worker.tsx
+++ EventHub/src/worker.tsx
@@ -1,6 +1,5 @@
 import { layout, render, route } from "rwsdk/router";
 import { defineApp } from "rwsdk/worker";
-
 import { Document } from "@/app/Document";
 import { setCommonHeaders } from "@/app/headers";
 import { Home } from "@/app/pages/Home";
@@ -14,14 +13,22 @@ import { Registration } from "./app/pages/Registration";
 import { Settings } from "./app/pages/Settings";
 import { Profile } from "./app/pages/Profile";
 import { Contact } from "./app/components/Contactus";
+import { setupDb, type DB } from "./db";
+import { env } from "cloudflare:workers";
 
-export type AppContext = {};
+export interface Env {
+  DB: D1Database;
+}
+
+export type AppContext = {
+  db: DB;
+};
 
 export default defineApp([
   setCommonHeaders(),
-  ({ ctx }) => {
-    // setup ctx here
-    ctx;
+  
+  async function setup({ctx}) {
+    ctx.db = await setupDb(env.DB)
   },
 
   render(Document, [
