a417bdee94ed706f20619666ef74ce698ca5bbfc
Amalie <iagresho@hiof.no>
2025-12-02 11:12:06 +0100

    opprydding

--- EventHub/src/app/pages/Event.tsx
+++ EventHub/src/app/pages/Event.tsx
@@ -1,7 +1,7 @@
 "use server"
 
 import type { RequestInfo } from "rwsdk/worker";
-import { EventDetail } from "../components/event-page/Event";
+import { EventDetail } from "../components/event/Event";
 
 export async function Event({ params} : RequestInfo) {
     return <EventDetail id={params.id} />;    
